ed4c66d23b2ea3637c7ebb586c891adbd1a32f07
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-03-30 18:29:47 -0600

    informacion

--- app/src/main/java/com/Laboratorios/lab2/ShowActivity.kt
+++ app/src/main/java/com/Laboratorios/lab2/ShowActivity.kt
@@ -3,6 +3,7 @@ package com.Laboratorios.lab2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_show.*
 
 class ShowActivity : AppCompatActivity() {
 
@@ -13,6 +14,25 @@ class ShowActivity : AppCompatActivity() {
         setContentView(R.layout.activity_show)
 
         var imc = intent.extras?.get("imc")
-        Toast.makeText(this, imc.toString(), Toast.LENGTH_LONG).show()
+        txtValor.setText("%.1f".format(imc))
+
+        imc = "%.1f".format(imc).toFloat()
+        if (imc <= 15.9){
+            // delgadez severa
+            txtMensaje.setText("Delgadez Severa")
+        }else if(imc >= 16.0 && imc <= 18.4){
+            // delgadez
+            txtMensaje.setText("Delgadez")
+        }else if (imc >= 18.5 && imc <= 24.9){
+            // saludable
+            txtMensaje.setText("Peso Saludable")
+        }else if(imc <= 25.0 && imc >= 29.9){
+            // sobrepeso
+            txtMensaje.setText("Sobrepeso")
+        }else if (imc >= 30.0){
+            // muy sobrepeso
+            txtMensaje.setText("Muy Sobrepeso")
+        }
+
     }
 }
